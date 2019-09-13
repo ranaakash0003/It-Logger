ba6ae4ec67744912878e8207e984596e0c975ee0
Rana Akash <rana.akash0003@gmail.com>
2019-09-13 16:31:25 +0600

    frontend with react completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect } from "react";
+import SearchBAr from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+import "./App.css";
 
-function App() {
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => {
+  useEffect(() => {
+    //Intit Materialize JS
+    M.AutoInit();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store= {store}>
+      <Fragment>
+        <SearchBAr />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/components/logs/AddLogModal.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const AddLogModal = () => {
+  const [message, setMessage] = useState("");
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState("");
+  const onSubmit = () => {
+    if(message === '' || tech === ''){
+      M.toast({ html: 'Please enter a message and tech' });
+    }
+    else{
+      console.log(message, tech, attention);
+    }
+  };
+
+  return (
+    <div id="add-log-modal" className="modal" style={modalStyle}>
+    <div className="modal-content">
+      <h4>Enter System Log</h4>
+      <div className="row">
+        <div className="input-field">
+          <input
+            type="text"
+            name="message"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+          <label htmlFor="message" className="active">message</label>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="input-field">
+          <select
+            name="tech"
+            value={tech}
+            className="browser-default"
+            onChange={e => setTech(e.target.value)}
+          >
+            <option value="" disabled>
+              Select Technitian
+            </option>
+            <option value="Rana Akash">Rana Akash</option>
+            <option value="Ariful Islam">Ariful Islam</option>
+            <option value="Rakib Hasan">Rakib Hasan</option>
+          </select>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field">
+          <p>
+            <label>
+              <input
+                type="checkbox"
+                className="filled-in"
+                checked={attention}
+                value={attention}
+                onChange={(e => setAttention(!attention))}
+              />
+              <span> Needs Attention</span>
+            </label>
+          </p>
+        </div>
+      </div>
+      <footer className="modal-footer">
+        <a
+          href="#!"
+          onClick={onSubmit}
+          className="modal-close waves-effect blue waves-green btn"
+        >
+          Enter
+        </a>
+      </footer>
+    </div>
+</div>
+  );
+};
+
+const modalStyle = {
+  width: "75%",
+  height: "75%"
+};
+
+export default AddLogModal;
